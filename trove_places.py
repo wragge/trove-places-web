d7d4e8c4e7593dac083f94bbe336d68ca6539d40
Tim Sherratt <tim@discontents.com.au>
2014-03-24 00:37:09 +1100

    Setting up MongoHQ

--- trove_places.py
+++ trove_places.py
@@ -1,10 +1,20 @@
+import os
+from urlparse import urlparse
 from flask import Flask
 from flask import request
 from flask import render_template, jsonify, Response
-from pymongo import MongoClient, GEO2D
+from pymongo import MongoClient, GEO2D, Connection
 from bson.json_util import dumps
 app = Flask(__name__)
 
+MONGO_URL = os.environ.get('MONGOHQ_URL')
+ 
+if MONGO_URL:
+	client = MongoClient(MONGO_URL)
+	db = client.get_default_database()
+else:
+	client = MongoClient()
+  	
 @app.route('/')
 def search():
     return render_template('search_form.html')
@@ -15,8 +25,6 @@ def get_place():
 	placename = request.args.get('name', None)
 	state = request.args.get('state', None)
 	if placename and state:
-		client = MongoClient()
-		db = client.trove_places
 		places = db.places
 		place = places.find({'name_lower': placename.lower(), 'state': state})
 	return Response(dumps(place), mimetype='application/json')
@@ -27,8 +35,6 @@ def find_near_titles():
 	lon = request.args.get('lon', None)
 	lat = request.args.get('lat', None)
 	if lon and lat:
-		client = MongoClient()
-		db = client.trove_places
 		titles = db.titles
 		titles.ensure_index([('places.loc', GEO2D)], min=-500, max=500)
 		near_titles = titles.find({"places.loc": {"$near": [float(lon), float(lat)]}}).limit(10)
